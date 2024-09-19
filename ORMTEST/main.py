6a2a65695b4179335f811dc45671be7dd54e6703
Lucas Maisonneuve <grouille666@hotmail.com>
2024-09-19 08:12:57 -0400

    Service ajouté: creerChambre, creerTypeChambre et créer client, changement dans le modele: les PK sont des UUID( Mapped[UUID] = mapped_column(default=uuid4,primary_key=True)) et DTO(data transfer object) ajouter au main.py.

--- ORMTEST/main.py
+++ ORMTEST/main.py
@@ -1,14 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from typing import Union
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 
-from modele.chambre import Chambre, Client, Reservation
+from modele.chambre import Chambre, Client, Reservation, TypeChambre
 
 engine = create_engine('mssql+pyodbc://GRINDLUPC\\SQLEXPRESS/Hotel?driver=SQL Server', use_setinputsizes=False)
 
 app = FastAPI()
+
+# Data Transfer Object : pydantic BaseModel pour intégration facile avec FastAPI
+class TypeChambreDTO(BaseModel): 
+    TYP_name: str
+    TYP_maxPrice: float
+    TYP_minPrice: float
+    TYP_description : str
+
+class ChambreDTO(BaseModel): 
+    CHA_roomNumber: int
+    CHA_availability : bool
+    CHA_otherInfo: str
+    type_chambre: str
+
+class ClientDTO(BaseModel):
+    CLI_nom: str
+    CLI_prenom: str
+    CLI_adresse: str
+    CLI_mobile: str
+    CLI_motDePasse: str
+    CLI_courriel: str
 
 @app.get("/chambre/{PKCHA_roomID}")
 def read_item(PKCHA_roomID: int):
@@ -22,6 +44,44 @@ def read_item(PKCHA_roomID: int):
                  "Type de Chambre" : chambre.Type_Chambre.TYP_name,
                  "ID": chambre.PKCHA_roomID
                 }
+
+@app.post("/creerchambre")
+def read_item(chambre: ChambreDTO):
+    with Session(engine) as session:
+        stmt = select(TypeChambre).where(TypeChambre.TYP_name == chambre.type_chambre)
+        result = session.execute(stmt)
+        print(stmt)      
+
+        for type_chambre in result.scalars():
+            print("test")
+
+            nouvelleChambre = Chambre(
+            CHA_roomNumber = chambre.CHA_roomNumber,
+            CHA_availability = chambre.CHA_availability,
+            CHA_otherInfo = chambre.CHA_otherInfo,
+            type_chambre = type_chambre
+            )
+
+            session.add(nouvelleChambre)
+            session.commit()
+        
+        return chambre
+
+@app.post("/creerTypeChambre")
+def read_item(type: TypeChambreDTO):
+    with Session(engine) as session:
+        nouveauTypeChambre = TypeChambre (
+            TYP_name = type.TYP_name,
+            TYP_maxPrice = type.TYP_maxPrice,
+            TYP_minPrice = type.TYP_minPrice,
+            TYP_description = type.TYP_description
+        )
+
+        session.add(nouveauTypeChambre)
+        session.commit()
+        
+        return type
+
 
 @app.get("/client/{CLI_nom}")
 def read_item(CLI_nom: str,CLI_prenom: Union[str] = None):
@@ -65,8 +125,20 @@ def read_item(CLI_nom: str,CLI_prenom: Union[str] = None):
                 "Mobile": result.CLI_mobile,
                 "Réservation": "None",
             }
-        
-        
 
-        
-  
+@app.post("/creerclient")
+def create_client(client_dto: ClientDTO):
+    with Session(engine) as session:
+            nouveau_client = Client(
+                CLI_nom=client_dto.CLI_nom,
+                CLI_prenom=client_dto.CLI_prenom,
+                CLI_adresse=client_dto.CLI_adresse,
+                CLI_mobile=client_dto.CLI_mobile,
+                CLI_motDePasse=client_dto.CLI_motDePasse,
+                CLI_courriel=client_dto.CLI_courriel
+            )
+            
+            session.add(nouveau_client)
+            session.commit()
+
+
--- ORMTEST/modele/chambre.py
+++ ORMTEST/modele/chambre.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy import DateTime 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, relationship, mapped_column
+from uuid import UUID, uuid4
 
 class Base(DeclarativeBase):
     pass
@@ -13,7 +14,7 @@ class Chambre(Base):
     CHA_roomNumber: Mapped[int]
     CHA_availability: Mapped[bool]
     CHA_otherInfo: Mapped[str]
-    PKCHA_roomID: Mapped[str] = mapped_column(primary_key=True)
+    PKCHA_roomID: Mapped[UUID] = mapped_column(default=uuid4,primary_key=True)
     FK_PKTYP_id: Mapped[str] = mapped_column(ForeignKey("Type_Chambre.PKTYP_id"))
 
     Type_Chambre: Mapped['TypeChambre'] = relationship()
@@ -25,7 +26,7 @@ class TypeChambre(Base):
     TYP_maxPrice: Mapped[float]
     TYP_minPrice: Mapped[float]
     TYP_description: Mapped[str]
-    PKTYP_id: Mapped[str] = mapped_column(primary_key=True)
+    PKTYP_id: Mapped[UUID] = mapped_column(default=uuid4,primary_key=True)
 
     chambres: Mapped[List["Chambre"]] = relationship(back_populates="Type_Chambre")
 
@@ -38,7 +39,7 @@ class Client(Base):
     CLI_mobile: Mapped[str]
     CLI_motDePasse: Mapped[str]
     CLI_courriel: Mapped[str]
-    PKCLI_id: Mapped[str] = mapped_column(primary_key=True)
+    PKCLI_id: Mapped[UUID] = mapped_column(default=uuid4,primary_key=True)
 
     Reservation: Mapped['Reservation'] = relationship()
 
@@ -49,7 +50,7 @@ class Reservation(Base):
     RES_endDate: Mapped[datetime]
     RES_pricePerDay: Mapped[float]
     RES_infoReservation: Mapped[str]
-    PKRES_id: Mapped[str] = mapped_column(primary_key=True)
+    PKRES_id: Mapped[UUID] = mapped_column(default=uuid4,primary_key=True)
     FK_PKCLI_id: Mapped[str] = mapped_column(ForeignKey("Client.PKCLI_id"))
     FK_PKCHA_roomID: Mapped[str] = mapped_column(ForeignKey("Chambre.PKCHA_roomID"))
 

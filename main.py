0a29cbdcecdb51e4555c8a251be27dedfa3ebb73
Catherine Beauchamp <36770490+insightboxCatherine@users.noreply.github.com>
2024-11-01 13:33:21 -0400

    Modification du service d'authentification.

--- main.py
+++ main.py
@@ -1,165 +1,191 @@
-from typing import Annotated
-from passlib.context import CryptContext
-import jwt
-from pydantic import BaseModel
-from datetime import datetime, timezone, timedelta
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from typing import Union
-from sqlalchemy import create_engine 
+from typing import Union, Annotated
+from pydantic import BaseModel
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from sqlalchemy import create_engine
+from datetime import datetime, timedelta, timezone
+import jwt
 import os
+
 from DTO.clientDTO import ClientDTO
-from metier.clientMetier import CreerClient,ChercherClient
+from metier.clientMetier import CreerClient,ChercherClient, ModifierClient
 from DTO.chambreDTO import ChambreDTO, TypeChambreDTO
 from metier.chambreMetier import CreerChambre, GetChambreParNumero, CreerTypeChambre, RechercherChambreLibre
 from DTO.reservationDTO import ReservationDTO
 from metier.reservationMetier import ModifierReservation, SupprimerReservation, CreerReservation, rechercherReservation
 
-engine = create_engine(f'mssql+pyodbc://{os.environ['COMPUTERNAME']}\\SQLEXPRESS/Hotel?driver=SQL+Server', use_setinputsizes=False)
+engine = create_engine(f'mssql+pyodbc://{os.environ['COMPUTERNAME']}\\SQLEXPRESS/Hotel?driver=SQL Server', use_setinputsizes=False)
 
-app = FastAPI()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-# Créez un contexte pour hacher les mots de passe
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# Clé secrète pour signer le JWT (gardez-la en sécurité !)
-SECRET_KEY = "votre_clé_secrète"
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-def hash_password(password: str):
-    return pwd_context.hash(password)
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": hash_password("secret")
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": hash_password("secret2")
-    },
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
 }
 
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-    if not verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-    return {"access_token": access_token, "token_type": "bearer"}
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-    return None
-
-def decode_token(token: str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
-        return get_user(fake_users_db, username)  # Utilise get_user pour obtenir l'utilisateur
-    except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
+class TokenData(BaseModel):
+    username: str | None = None
 
 class User(BaseModel):
     username: str
     email: str | None = None
     full_name: str | None = None
+    disabled: bool | None = None
 
 class UserInDB(User):
     hashed_password: str
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
 @app.get("/chambre/{No_chambre}")
-def read_item(CHA_roomNumber: int, current_user: Annotated[User, Depends(get_current_user)]):
+def read_item(current_user: Annotated[User, Depends(get_current_active_user)], CHA_roomNumber: int):
     return GetChambreParNumero(CHA_roomNumber)
 
-@app.post("/creerTypeChambre")
-def read_item(type_dto: TypeChambreDTO, current_user: Annotated[User, Depends(get_current_user)]):
-    return CreerTypeChambre(type_dto)    
-
 @app.post("/creerchambre")
-def read_item(chambre: ChambreDTO, current_user: Annotated[User, Depends(get_current_user)]):
+def read_item(current_user: Annotated[User, Depends(get_current_active_user)], chambre: ChambreDTO):
     return CreerChambre(chambre)
 
+@app.post("/creerTypeChambre")
+def read_item(current_user: Annotated[User, Depends(get_current_active_user)], type_dto: TypeChambreDTO):
+    return CreerTypeChambre(type_dto)
+
 @app.get("/client/{CLI_nom}")
-def read_item(CLI_nom: str, CLI_prenom: Union[str, None] = None, current_user: User = Depends(get_current_user)):
+def read_item(current_user: Annotated[User, Depends(get_current_active_user)], CLI_nom: str,CLI_prenom: Union[str] = None):
     return ChercherClient(CLI_nom)
 
 @app.post("/creerclient")
-def create_client(client_dto: ClientDTO, current_user: Annotated[User, Depends(get_current_user)]):
+def create_client(current_user: Annotated[User, Depends(get_current_active_user)], client_dto: ClientDTO):
     return CreerClient(client_dto)
 
 @app.post("/modifierreservation")
-def modify_reservation(CLI_nom : str, reservation : ReservationDTO, current_user: Annotated[User, Depends(get_current_user)]):
-    return ModifierReservation(CLI_nom, reservation)
+def modify_reservation(current_user: Annotated[User, Depends(get_current_active_user)], PKRES_id: str, reservation : ReservationDTO):
+    return ModifierReservation(PKRES_id, reservation)
 
 @app.post("/supprimerreservation")
-def delete_reservation(CLI_nom : str, current_user: Annotated[User, Depends(get_current_user)]):
-    return SupprimerReservation(CLI_nom)
+def delete_reservation(current_user: Annotated[User, Depends(get_current_active_user)], PKRES_id: str):
+    return SupprimerReservation(PKRES_id)
 
 @app.post("/creerreservation")
-def Creer_reservation(CLI_nom: str, CHA_roomNumber,reservation: ReservationDTO, current_user: Annotated[User, Depends(get_current_user)]):
+def Creer_reservation(current_user: Annotated[User, Depends(get_current_active_user)], CLI_nom: str, CHA_roomNumber,reservation: ReservationDTO):
     return CreerReservation(CLI_nom, CHA_roomNumber, reservation)
 
+@app.post("/modifierclient/{client_id}")
+def modifier_client(current_user: Annotated[User, Depends(get_current_active_user)], client_id: str, client_dto: ClientDTO):
+    return ModifierClient(client_id, client_dto)
+
 @app.post("/rechercherReservation")
-def rechercher_reservations(
-    prenom: Union[str, None] = None,
-    nom: Union[str, None] = None,
-    roomNumber: Union[int, None] = None,
-    idClient: Union[str, None] = None,
-    idReservation: Union[str, None] = None,
-    startDate: Union[datetime, None] = None,
-    endDate: Union[datetime, None] = None,
-    current_user: User = Depends(get_current_user)  
-):
+def rechercher_reservations(current_user: Annotated[User, Depends(get_current_active_user)], prenom: Union[str]=None, nom: Union[str]=None, roomNumber: Union[int]=None, idClient: Union[str]=None, idReservation:Union[str]=None, startDate:Union[datetime]=None, endDate:Union[datetime]=None):
     try:
         return rechercherReservation(prenom, nom, roomNumber, idClient, idReservation, startDate, endDate)
     except ValueError as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        return HTTPException(status_code=404, detail=str(e))
 
 @app.post("/rechercherchambrelibre")
-def rechercher_chambre_libre(current_user: User = Depends(get_current_user)):
+def rechercher_chambre_libre(current_user: Annotated[User, Depends(get_current_active_user)]):
     return RechercherChambreLibre()
- 
-
  
   
 
